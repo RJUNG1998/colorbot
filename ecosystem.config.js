9679db11f1eeac633647cfd72b9b4c8736d2a93d
RJUNG1998 <jung.ray21@gmail.com>
2023-04-20 09:01:13 -0400

    ecosystem added for pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    apps : [{
+      name   : "colorbot",
+      script : "src/bot.js",
+      env_production: {
+        TOKEN: "MTA3MzQ5NDUyNjE5MjI1NTAxNw.Gh6DOG.AQTpnFjujockFfVPPM-Z77kTJygUYihPw9k_Vs",
+        CLIENT_ID: "1073494526192255017",
+        GUILD_ID: "1032038449886265404"
+      }
+    }]
+  }
