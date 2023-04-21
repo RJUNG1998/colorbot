e41dd5f98892d5a21fbeeda07c6438d7b9b92c90
RJUNG1998 <jung.ray21@gmail.com>
2023-04-21 15:16:48 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       env_production: {
         TOKEN: "MTA3MzQ5NDUyNjE5MjI1NTAxNw.Gh6DOG.AQTpnFjujockFfVPPM-Z77kTJygUYihPw9k_Vs",
         CLIENT_ID: "1073494526192255017",
-        GUILD_ID: "1032038449886265404"
+        GUILD_ID: "937556248847581244"
       }
     }]
   }
