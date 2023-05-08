0b9602acaf111112c5585b40e2794ef86c774f33
RJUNG1998 <jung.ray21@gmail.com>
2023-05-08 05:50:39 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
       env_production: {
         TOKEN: "MTA3MzQ5NDUyNjE5MjI1NTAxNw.Gh6DOG.AQTpnFjujockFfVPPM-Z77kTJygUYihPw9k_Vs",
         CLIENT_ID: "1073494526192255017",
-        GUILD_ID: "937556248847581244"
+        GUILD_ID: "937556248847581244",
+        DATABASE_TOKEN: "mongodb+srv://adamzzang1357:adam242529@colorbot.6sf3cl1.mongodb.net/?retryWrites=true&w=majority",
       }
     }]
   }
