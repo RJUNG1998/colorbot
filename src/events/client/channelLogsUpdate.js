267f68d84a253f3a9c8b9de045ec19138f78bb17
Icle_ <evansyjung30518@gmail.com>
2023-05-23 17:17:35 -0700

    Update channelLogsUpdate.js

--- src/events/client/channelLogsUpdate.js
+++ src/events/client/channelLogsUpdate.js
@@ -3,7 +3,7 @@ const { EmbedBuilder } = require("discord.js")
 module.exports = {
     name: 'channelUpdate',
     async execute(oldChannel, newChannel, client) {
-        const logChannelId = '1099666225472208947'
+        const logChannelId = '938210526994006076'
         const logChannel = client.channels.cache.get(logChannelId)
         
         // Voice Channel
