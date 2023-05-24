47459c7da57e4140969d2b494a96cd6b7391e4aa
Icle_ <evansyjung30518@gmail.com>
2023-05-23 17:36:56 -0700

    d

--- src/events/client/channelLogsDelete.js
+++ src/events/client/channelLogsDelete.js
@@ -28,3 +28,21 @@ module.exports = {
         // }
     }
 }
+
+module.exports = {
+    name: 'channelUpdate',
+    async execute(oldChannel, newChannel, client) {
+        const logChannelId = '938210526994006076'
+        const logChannel = client.channels.cache.get(logChannelId)
+        
+        // Voice Channel
+        if (client.channels.cache.get(newChannel.id).type === 2) {
+            logChannel.send({ embeds: [
+                new EmbedBuilder()
+                .setColor("Blue")
+                .setDescription(`**${oldChannel.name}** 채널이 <#${newChannel.id}> 으로 채널명 **변경**!`)
+                .setTimestamp()
+            ]})
+        }
+    }
+}
