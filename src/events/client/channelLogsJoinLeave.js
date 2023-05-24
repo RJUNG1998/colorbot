e0cae2fdbc2b2aa3ddc55d690311158ea182c9d6
Icle_ <evansyjung30518@gmail.com>
2023-05-23 17:17:41 -0700

    Update channelLogsJoinLeave.js

--- src/events/client/channelLogsJoinLeave.js
+++ src/events/client/channelLogsJoinLeave.js
@@ -4,7 +4,7 @@ module.exports = {
     name: 'voiceStateUpdate',
     async execute(oldState, newState, client) {
 
-        const logChannelId = '1099666225472208947'
+        const logChannelId = '938210526994006076'
         const logChannel = client.channels.cache.get(logChannelId)
 
         const guildId = newState.guild.id;
@@ -13,37 +13,37 @@ module.exports = {
         //봇 일때 return
         if (newState.member.user.bot) return;
 
-        // if (oldState.channelId && !newState.channelId || (oldState.channelId && newState.channelId && (oldState.channelId !== newState.channelId)) && newState.channelId !== "1099655403580702760") {
-        //     //완전 퇴장
-        //     const channel = oldState.guild.channels.cache.get(oldState.channelId)
-        //     const startTime = client.logTotalTime.get(`${guildId}-${userId}`)
-        //     if (startTime) {
-        //         const endTime = Date.now();
-        //         const totalTimeInMs = endTime - startTime;
+        if (oldState.channelId && !newState.channelId || (oldState.channelId && newState.channelId && (oldState.channelId !== newState.channelId)) && newState.channelId !== "1099655403580702760") {
+            //완전 퇴장
+            const channel = oldState.guild.channels.cache.get(oldState.channelId)
+            const startTime = client.logTotalTime.get(`${guildId}-${userId}`)
+            if (startTime) {
+                const endTime = Date.now();
+                const totalTimeInMs = endTime - startTime;
 
-        //         let totalTimeInSeconds = Math.floor(totalTimeInMs / 1000);
-        //         let totalTimeInMinutes = Math.floor(totalTimeInSeconds / 60);
-        //         let totalTimeInHours = Math.floor(totalTimeInMinutes / 60);
+                let totalTimeInSeconds = Math.floor(totalTimeInMs / 1000);
+                let totalTimeInMinutes = Math.floor(totalTimeInSeconds / 60);
+                let totalTimeInHours = Math.floor(totalTimeInMinutes / 60);
 
-        //         totalTimeInMinutes %= 60;
-        //         totalTimeInSeconds %= 60;
+                totalTimeInMinutes %= 60;
+                totalTimeInSeconds %= 60;
             
-        //         logChannel.send({ embeds: [
-        //             new EmbedBuilder()
-        //             .setColor("Red")
-        //             .setDescription(`<@${oldState.member.id}>님이 <#${channel.id}>에서 **퇴장**!`)
-        //             .setTimestamp()
-        //             .setFooter({ text: `${totalTimeInHours}시간 ${totalTimeInMinutes}분 ${totalTimeInSeconds}초 동안 있었어요!`, iconURL: 'https://i.imgur.com/nrHdpDL.png'})
-        //         ]})
+                logChannel.send({ embeds: [
+                    new EmbedBuilder()
+                    .setColor("Red")
+                    .setDescription(`<@${oldState.member.id}>님이 <#${channel.id}>에서 **퇴장**!`)
+                    .setTimestamp()
+                    .setFooter({ text: `${totalTimeInHours}시간 ${totalTimeInMinutes}분 ${totalTimeInSeconds}초 동안 있었어요!`, iconURL: 'https://i.imgur.com/nrHdpDL.png'})
+                ]})
 
-        //         client.logTotalTime.delete(`${guildId}-${userId}`)
-        //     }
-        //     client.logTotalTime.set(`${guildId}-${userId}`, Date.now())
-        // }
+                client.logTotalTime.delete(`${guildId}-${userId}`)
+            }
+            client.logTotalTime.set(`${guildId}-${userId}`, Date.now())
+        }
         // 입장문
         if (newState.channelId && !oldState.channelId || (oldState.channelId && newState.channelId && (oldState.channelId !== newState.channelId))) {
             const channel = newState.guild.channels.cache.get(newState.channelId)
-            if (channel.id !== "1099655403580702760") {
+            if (channel.id !== "938210526994006076") {
                 logChannel.send({ embeds: [
                     new EmbedBuilder()
                     .setColor("Purple")
@@ -82,32 +82,32 @@ module.exports = {
             // }
         // } 
         
-        // // 이동문
-        // if (newState.channelId && oldState.channel) {
-        //     if (newState.channel.id !== oldState.channel.id) {
-        //         const startTime = client.logTotalTime.get(`${guildId}-${userId}`)
-        //         if (startTime) {
-        //             const endTime = Date.now();
-        //             const totalTimeInMs = endTime - startTime;
+        // 이동문
+        if (newState.channelId && oldState.channel) {
+            if (newState.channel.id !== oldState.channel.id) {
+                const startTime = client.logTotalTime.get(`${guildId}-${userId}`)
+                if (startTime) {
+                    const endTime = Date.now();
+                    const totalTimeInMs = endTime - startTime;
 
-        //             let totalTimeInSeconds = Math.floor(totalTimeInMs / 1000);
-        //             let totalTimeInMinutes = Math.floor(totalTimeInSeconds / 60);
-        //             let totalTimeInHours = Math.floor(totalTimeInMinutes / 60);
+                    let totalTimeInSeconds = Math.floor(totalTimeInMs / 1000);
+                    let totalTimeInMinutes = Math.floor(totalTimeInSeconds / 60);
+                    let totalTimeInHours = Math.floor(totalTimeInMinutes / 60);
 
-        //             totalTimeInMinutes %= 60;
-        //             totalTimeInSeconds %= 60;
+                    totalTimeInMinutes %= 60;
+                    totalTimeInSeconds %= 60;
 
-        //             logChannel.send({ embeds: [
-        //                 new EmbedBuilder()
-        //                 .setColor("Purple")
-        //                 .setDescription(`<@${oldState.member.id}>님이 <#${oldState.channelId}>에서 <#${newState.channelId}>로 이동!`)
-        //                 .setTimestamp()
-        //                 .setFooter({ text: `${totalTimeInHours}시간 ${totalTimeInMinutes}분 ${totalTimeInSeconds}초 동안 있었어요!`, iconURL: 'https://i.imgur.com/nrHdpDL.png'})]
-        //             })
+                    logChannel.send({ embeds: [
+                        new EmbedBuilder()
+                        .setColor("Purple")
+                        .setDescription(`<@${oldState.member.id}>님이 <#${oldState.channelId}>에서 <#${newState.channelId}>로 이동!`)
+                        .setTimestamp()
+                        .setFooter({ text: `${totalTimeInHours}시간 ${totalTimeInMinutes}분 ${totalTimeInSeconds}초 동안 있었어요!`, iconURL: 'https://i.imgur.com/nrHdpDL.png'})]
+                    })
 
-        //         client.logTotalTime.set(`${guildId}-${userId}`, Date.now())
-        //         }
-        //     }
-        // }
+                client.logTotalTime.set(`${guildId}-${userId}`, Date.now())
+                }
+            }
+        }
     }
 }
