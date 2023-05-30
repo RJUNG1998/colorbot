4924c4ce48d7176c1ecf525f6a3c1c0d7d401fc9
RJUNG1998 <jung.ray21@gmail.com>
2023-05-30 02:45:44 -0400

    Update welcome.js

--- src/events/client/welcome.js
+++ src/events/client/welcome.js
@@ -1,10 +1,19 @@
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
-        let welcomeRole = member.guild.roles.cache.find(role => role.name === '🚩승인필요');
-        let welcomeRole2 = member.guild.roles.cache.find(role => role.name === '------------------<  소개  >------------------');
-        member.roles.add(welcomeRole)
-        member.roles.add(welcomeRole2)
+        const introRole = "951197966641803315";
+        const gameRole = "1021230808914206760";
+        const customRole = "1105028550437904455";
+        const hobbyRole = "951197274086723695";
+        const warningRole = "1047622259155677364";
+        const acceptRole = "937562805702393898";
+
+        member.roles.add(introRole);
+        member.roles.add(gameRole);
+        member.roles.add(customRole);
+        member.roles.add(hobbyRole);
+        member.roles.add(warningRole);
+        member.roles.add(acceptRole);
     
         const embed = {
             title: '🖌️ PALETTE 🎨\n\n- 승인 절차 -',
