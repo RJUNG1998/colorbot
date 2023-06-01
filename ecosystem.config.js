e58e623270101790b5be4f1e17ee59c7ec2070c0
RJUNG1998 <jung.ray21@gmail.com>
2023-05-31 21:06:51 -0400

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,6 +7,8 @@ module.exports = {
         CLIENT_ID: "1073494526192255017",
         GUILD_ID: "937556248847581244",
         DATABASE_TOKEN: "mongodb+srv://adamzzang1357:adam242529@colorbot.6sf3cl1.mongodb.net/?retryWrites=true&w=majority",
+        AWS_ACCESS_KEY_ID: "AKIAS3Q4EKLQDSR3ITYV",
+        AWS_SECRET_ACCESS_KEY: "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"
       }
     }]
   }
