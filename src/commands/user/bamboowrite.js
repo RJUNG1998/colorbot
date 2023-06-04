23afe0957515edc3e99486b90f70ef4478364c1f
Icle_ <evansyjung30518@gmail.com>
2023-06-04 00:53:11 -0700

    .
    .

--- src/commands/user/bamboowrite.js
+++ src/commands/user/bamboowrite.js
@@ -1,5 +1,5 @@
 const { SlashCommandBuilder, ActionRowBuilder, TextInputBuilder, ModalBuilder, TextInputStyle } = require('discord.js');
-const Embeds = require('../../class/embeds');
+const Embeds = require('../src/class/embeds');
 
 module.exports = {
     data: new SlashCommandBuilder()
